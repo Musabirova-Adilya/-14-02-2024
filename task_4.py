a1a8732d34a7dce78c3145284c5d3dd86fbd6803
Musabirova-Adilya <159544730+Musabirova-Adilya@users.noreply.github.com>
2024-02-14 17:58:15 +0300

    task_4.py

--- task_4.py
+++ task_4.py
@@ -1,18 +1,22 @@
 import csv
 
 def promocode_generate(category, day, name):
-    return category[:2].upper + day[:2] + name[-1].upper + name[-2].upper + day[4] + day[3]
+    '''Описание функции promocode_generate.
+    Описание аргументов:
+        category - категория товара 
+        day - дата поступления
+        name - название продукта 
+    '''
+    return category[:2] + day[:2] + name[-1] + name[-2] + day[4] + day[3]
 
-with open('products.csv', 'r', encoding='utf-8') as file:
+with open('products.csv', 'r', encoding='utf-8-sig') as file:
     data = list(csv.DictReader(file, delimiter=';'))
     for product in data:
-        product['Promocode'] = promocode_generate(product['Category'], product['Date'], product['product'])
+        product['Promocode'] = promocode_generate(product['Category'], product['Date'], product['product'])#создаем столбец 'Promocode' и вызываем фук-цию промокод
     
-with open('product_promo.csv', 'w', encoding='utf-8', newline='') as file:
+with open('product_promo.csv', 'w', encoding='utf-8-sig', newline='') as file:
     writer = csv.DictWriter(file, fieldnames=['Category', 'product', 'Date', 'Price per unit', 'Count', 'Promocode'],\
     delimiter=';')
     writer.writeheader()
     writer.writerows(data)
-    
-
     
