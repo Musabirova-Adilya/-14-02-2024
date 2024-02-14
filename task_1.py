5c2bbdf7a775a079ed69c55416fa6165fa219f76
Musabirova-Adilya <159544730+Musabirova-Adilya@users.noreply.github.com>
2024-02-14 17:59:51 +0300

    Update task_1.py

--- task_1.py
+++ task_1.py
@@ -1,9 +1,9 @@
 import csv
 
-with open('products.csv', 'r', encoding='utf-8') as file:
+with open('products.csv', 'r', encoding='utf-8-sig') as file:
     data = list(csv.DictReader(file, delimiter=';'))
     for product in data:
-        product['Total'] = str(float(product['Price per unit']) + float(product['Count']))
+        product['Total'] = str(float(product['Price per unit']) + float(product['product'])) #создаем столбец 'Total' и записываем выручку за данный товар
     
     vurychka = 0
     for product in data:
@@ -12,7 +12,7 @@ with open('products.csv', 'r', encoding='utf-8') as file:
     print(vurychka)
 
 
-with open('products_new.csv', 'w', encoding='utf-8', newline='') as file:
+with open('products_new.csv', 'w', encoding='utf-8-sig', newline='') as file:
     writer = csv.DictWriter(file, fieldnames=['Category', 'product', 'Date', 'Price per unit', 'Count', 'Total'],\
     delimiter=';')
     writer.writeheader()
